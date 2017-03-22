2780810d6b8a1e77d4eb0842e39a1cb8caed87dc
mjhrich <mjhrich@gmail.com>
2017-03-22 17:32:01 -0400

    Added masonry, updated images
    Big update 2. Refined styles, adjusted pages. Wrote copy. Started image
    linking. Masonry not working on image page yet.

---
+++ js/info.js
@@ -0,0 +1,14 @@
+$(function(){
+
+
+$('.grid').masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
+
+// $grid.imagesLoaded().progress(function() {
+//   $grid.masonry('layout');
+// });
+
+});
